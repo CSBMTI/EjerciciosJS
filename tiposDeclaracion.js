abadc497beeaeb05c0189c3e738c3a3c68ceb258
CSBM-T.I <csbadministracion@csblasalle.edu.mx>
2021-03-30 14:02:58 -0600

    Subiendo archivos desde visual studio code

---
+++ tiposDeclaracion.js
@@ -0,0 +1,22 @@
+//Declaración 5 datos característicos de un coche
+//var marca="Ford";
+//var color="rojo";
+const anio=2000;
+var placa="HDF-556";
+const modelo="Corola";
+const marca="Ford";
+const paisOrigen="Japón";
+console.log( anio );
+
+var booleanoVerdadero="true";
+var booleanoFalso="falso";
+
+var numero1=1;
+var numero2=2;
+
+var texto="Bicicleta";
+var texto2="Bienvenido al curso";
+
+var condicion=null;
+var valorIndefinido;
+console.log( valorIndefinido );
